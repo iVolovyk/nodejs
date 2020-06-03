e5f58fe737256c0bf683954d0d36646a05bafa8f
Ihor Volovyk <ihor.volovyk@wesoftyou.com>
2020-06-03 22:34:23 +0300

    hw5 done

--- api/users/user.model.js
+++ api/users/user.model.js
@@ -9,6 +9,7 @@ const userSchema = new Schema({
     enum: ['free', 'pro', 'premium'],
     default: 'free',
   },
+  avatarURL: String,
   token: String,
 });
 
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,11 +1,37 @@
 const { Router } = require('express');
+const multer = require('multer');
+const path = require('path');
+
 const userController = require('./user.controller');
 
 const authRouter = Router();
 const userRouter = Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'tmp');
+  },
+  filename: (req, file, cb) => {
+    if (!file.mimetype.includes('image')) {
+      const err = new Error();
+      err.status = 400;
+      return cb(err);
+    }
+
+    const originName = file.originalname;
+    const pathParse = path.parse(originName);
+    const fileWithFormat = `${Date.now()}${pathParse.ext}`;
+    cb(null, fileWithFormat);
+  },
+});
+
+const upload = multer({ storage });
+
 authRouter.post(
   '/register',
+  userController.checkExistingUser,
+  userController.createAvatar,
+  userController.createAvatarURL,
   userController.validateUserCredentials,
   userController.createUser,
 );
@@ -25,12 +51,19 @@ userRouter.get(
   userController.authorize,
   userController.refreshUser,
 );
-
 userRouter.patch(
   '/',
   userController.authorize,
   userController.validateUpdateUser,
   userController.updateUserSub,
+);
+userRouter.patch(
+  '/avatars',
+  userController.authorize,
+  upload.single('avatar'),
+  userController.compressImage,
+  userController.createUpdatedAvatarURL,
+  userController.updateUserAvatar,
 );
 
 module.exports = { authRouter, userRouter };
